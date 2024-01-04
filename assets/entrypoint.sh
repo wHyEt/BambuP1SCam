d4537be5e8c43cab9c2b7c7300d963b58061ca82
wHyEt <github@tech-nic.eu>
2024-01-05 00:03:00 +0100

    Added Debug Entrypoint for testing

--- assets/entrypoint.sh
+++ assets/entrypoint.sh
@@ -2,8 +2,8 @@
 
 set -e
 
-3DPrinter_IP=${3DPrinter_IP}
-3DPrinter_CODE=${3DPrinter_CODE}
+3DPrinter_IP=${3DPrinter_IP:0.0.0.0}
+3DPrinter_CODE=${3DPrinter_CODE:xxxxxx}
 
 mkdir work
 cd work
@@ -13,3 +13,4 @@ unzip linux_01.04.00.15.zip
 
 wget https://github.com/AlexxIT/go2rtc/releases/download/v1.6.2/go2rtc_linux_amd64
 chmod a+x go2rtc_linux_amd64
+
